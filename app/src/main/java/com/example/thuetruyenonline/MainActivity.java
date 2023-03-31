2e5b9c60f97b8cc8a300bff2b29faffbf2621895
An 8 Bit <106478425+An8bit@users.noreply.github.com>
2023-03-31 14:33:00 +0700

    Update MainActivity.java
    ok

--- app/src/main/java/com/example/thuetruyenonline/MainActivity.java
+++ app/src/main/java/com/example/thuetruyenonline/MainActivity.java
@@ -43,7 +43,7 @@ public class MainActivity extends AppCompatActivity {
 //                            String tk = docu.get("TaiKhoan").toString();
 //                            String pa =docu.get("matkhau").toString();
                             String anh = docu.get("AnhLoad").toString();
-                            Glide.with(MainActivity.this).load("https://www.google.com/imgres?imgurl=https%3A%2F%2Fhoathinh3d.com%2Fwp-content%2Fuploads%2F2022%2F09%2Fdau-pha-thuong-khung-phan-5-gia-nam-hoc-vien-1321.jpg&tbnid=pLBblBzL3xk99M&vet=12ahUKEwiC34CvyoX-AhUvpVYBHU4-DFUQMygAegUIARDGAQ..i&imgrefurl=https%3A%2F%2Fhoathinh3d.com%2Fdau-pha-thuong-khung-phan-5-gia-nam-hoc-vien&docid=k9cVoj3pnLEmLM&w=1335&h=2048&q=%C4%91%C3%A1u%20ph%C3%A1%20th%C6%B0%C6%A1ng%20khung%20p5&ved=2ahUKEwiC34CvyoX-AhUvpVYBHU4-DFUQMygAegUIARDGAQ").into(image);
+                            Glide.with(MainActivity.this).load("").into(image);
                     }
                 }
 
