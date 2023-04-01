72b26f177f8392e1cae7903ee41f062e04e7d50e
An8bit <ttkit35@gmail.com>
2023-04-01 13:19:04 +0700

    xong Login.java

--- app/src/main/java/com/example/thuetruyenonline/Login.java
+++ app/src/main/java/com/example/thuetruyenonline/Login.java
@@ -10,18 +10,24 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageButton;
+import android.widget.Toast;
 
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.OnFailureListener;
 import com.google.android.gms.tasks.Task;
+import com.google.firebase.firestore.CollectionReference;
 import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.Query;
 import com.google.firebase.firestore.QueryDocumentSnapshot;
 import com.google.firebase.firestore.QuerySnapshot;
 
+import java.util.Collection;
+
 public class Login extends AppCompatActivity {
 
+
     FirebaseFirestore db ;
-    EditText e,p;
+    EditText email,pass;
     Button Login;
     @SuppressLint("MissingInflatedId")
     @Override
@@ -29,32 +35,32 @@ public class Login extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
         db=FirebaseFirestore.getInstance();
-        e=findViewById(R.id.etEmail);
-        p=findViewById(R.id.etPass);
+        email=findViewById(R.id.etEmail);
+        pass=findViewById(R.id.etPass);
         Login=findViewById(R.id.btLogin);
-        db.collection("TaiKhoan")
-                .get().addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
-                    @Override
-                    public void onComplete(@NonNull Task<QuerySnapshot> task) {
-                        for (QueryDocumentSnapshot docu:task.getResult()){
-                            String tk = docu.get("TaiKhoan").toString();
-                            String pa =docu.get("matkhau").toString();
-                            e.setHint(tk);
-                            p.setHint(pa);
-                        }
-                    }
+        //đọc document TaiKhoan gán vào biến usersRef
+        CollectionReference usersRef = db.collection("TaiKhoan");
 
-                }).addOnFailureListener(new OnFailureListener() {
-                    @Override
-                    public void onFailure(@NonNull Exception e) {
-
-                    }
-                });
         Login.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Intent intent = new Intent(Login.this,MainActivity.class);
-                startActivity(intent);
+                //tạo một truy vấn đến đến từng dòng ở đây sử dụng query lấy làm whereEqualTo tìm kiếm lọc dữ liệu
+                Query query = usersRef.whereEqualTo("matkhau",pass.getText().toString()).whereEqualTo("taikhoan",email.getText().toString());
+                query.get().addOnCompleteListener(task -> {
+                    //kiểm tra trong task có truy vấn thành công không
+                    if (task.isSuccessful()){
+                        QuerySnapshot snapshots = task.getResult();
+                        //kiểm tra dữ liệu có rỗng hay không
+                        if(snapshots.isEmpty()){
+                            Toast.makeText(Login.this,"Tài khoản hoặc mật khẩu không đúng",Toast.LENGTH_LONG).show();
+                        }else {
+                            Intent intent = new Intent(Login.this,MainActivity.class);
+                            startActivity(intent);
+                        }
+                    }else {
+                        Toast.makeText(Login.this,"error",Toast.LENGTH_SHORT).show();
+                    }
+                });
             }
         });
     }
